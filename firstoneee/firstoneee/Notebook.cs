8a4878ade652e9e5eae975ffe394ec7141f2be25
Allahverdi Asgarli <allahverdiaa@code.edu.az>
2022-05-30 00:40:27 -0700

    justdidonetasksorry

---
+++ firstoneee/firstoneee/Notebook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace firstoneee
+{
+    class Notebook
+    {
+        public string Name;
+        public string Brand;
+        public byte Price;
+
+
+    }
+}
---
+++ firstoneee/firstoneee/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace firstoneee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Notebook notebook1 = new Notebook()
+            {
+                Name = "PC",
+                Brand = "Lenovo",
+                Price = 250,
+            };
+
+            Console.WriteLine(notebook1.Name);
+            Console.WriteLine(notebook1.Brand);
+            Console.WriteLine(notebook1.Price);
+
+
+
+
+
+
+
+        }
+    }
+}
